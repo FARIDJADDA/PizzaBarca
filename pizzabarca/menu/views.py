c0168f7e598e7c1b32d58f3c44043359066eadad
Farid Djadda <djaddafarid.contact@gmail.com>
2024-12-02 13:14:55 +0100

    feat(menu): fetch and display pizzas from database
    Updated the 'index' view to fetch all Pizza objects from the database.
    - Retrieved all pizzas using the Pizza model.
    - Constructed a list of pizza names and joined them into a string.
    - Updated the HTTP response to display the list of pizzas.
    
    This change enables dynamic data fetching for the menu.

--- pizzabarca/menu/views.py
+++ pizzabarca/menu/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Pizza
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Les pizzas")
+    pizzas = Pizza.objects.all()
+    pizzas_names = [pizza.name for pizza in pizzas]
+    display_pizzas = ", ".join(pizzas_names)
+    return HttpResponse("Les pizzas : " + display_pizzas)
